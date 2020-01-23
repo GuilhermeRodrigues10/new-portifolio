5d14802943b619497a348887269aee39b9280594
Guilherme <guilherme.santos@ringa.com.br>
2020-01-23 16:28:02 -0300

    Create the Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Sidebar from './components/Sidebar.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi There</h1>
+    <div>
+      <Sidebar color="red" />
     </div>
   );
 }
