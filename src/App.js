0b9901fd982b2916632a75c3dbb9833de9a04082
Guilherme <guilherme.santos@ringa.com.br>
2020-01-24 15:20:57 -0300

    Add bootstrap and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Sidebar from './components/Sidebar.js'
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Sidebar color="red" />
+      <Container>
+        <Row>
+          <Col xs={3}><Sidebar /></Col>
+          <Col xs={9}>2 of 3 (wider)</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
