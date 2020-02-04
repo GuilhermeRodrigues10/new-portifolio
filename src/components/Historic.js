6a5c54605e0772875ca6e7fa94eeb43b8a9110c3
Guilherme <guilherme.santos@ringa.com.br>
2020-02-04 15:02:52 -0300

    Aplly fadedin for Historic page

--- src/components/Historic.js
+++ src/components/Historic.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import FadeIn from "react-fade-in";
 
 import HistoricCard from "./HistoricCard.js";
 import Sidebar from "./Sidebar.js";
@@ -20,38 +21,52 @@ class Historic extends React.Component {
               <Sidebar />
             </Col>
             <Col xs={8}>
-              <HistoricCard
-                text="Comecei minha jornada como Atendente de Call
-                Center e desde Janeiro de 2020 trabalho como Supervisor de
-                Operações."
-                pic={mapfre}
-                year="Dez 2013 - Atual"
-              />
-              <HistoricCard
-                text="Atualmente trabalho com Desenvolvimento Web utilizando as mais
-                modernas Linguagens (React e Ruby on Rails) e ferramentas de
-                Desenvolvimento Ágil (Scrum e Kanban)."
-                pic={ringa}
-                year="Set 2018 - Atual"
-              />
-              <HistoricCard
-                text="A Cati Jr é uma Empresa Junior criada e administrada exclusivamente
-                por alunos da Universidade Federal de São Carlos. Trabalhei como Product Owner
-                e pude ter um primeiro contato com o universo empreendedor."
-                pic={catijr}
-                year="Mar 2018 - Set 2018"
-              />
-              <HistoricCard
-                text="Atualmente estou cursando Bacharelado em Ciência da Computação."
-                pic={ufscar}
-                year="Mar 2013 - Ago 2020"
-              />
-              <HistoricCard
-                text="Ensino Médio integrado com o Ensino Técnico em Informática.
-                Neste ponto que escolhi qual carreira seguiria após o colegial."
-                pic={etec}
-                year="Mar 2010 - Dez 2012"
-              />
+              <FadeIn delay={50} >
+                <HistoricCard
+                  text="Comecei minha jornada como Atendente de Call
+                  Center e desde Janeiro de 2020 trabalho como Supervisor de
+                  Operações."
+                  pic={mapfre}
+                  year="Dez 2013 - Atual"
+                />
+              </FadeIn>
+
+              <FadeIn delay={150} >
+                <HistoricCard
+                  text="Atualmente trabalho com Desenvolvimento Web utilizando as mais
+                  modernas Linguagens (React e Ruby on Rails) e ferramentas de
+                  Desenvolvimento Ágil (Scrum e Kanban)."
+                  pic={ringa}
+                  year="Set 2018 - Atual"
+                />
+              </FadeIn>
+
+              <FadeIn delay={250} >
+                <HistoricCard
+                  text="A Cati Jr é uma Empresa Junior criada e administrada exclusivamente
+                  por alunos da Universidade Federal de São Carlos. Trabalhei como Product Owner
+                  e pude ter um primeiro contato com o universo empreendedor."
+                  pic={catijr}
+                  year="Mar 2018 - Set 2018"
+                />
+              </FadeIn>
+
+              <FadeIn delay={350} >
+                <HistoricCard
+                  text="Atualmente estou cursando Bacharelado em Ciência da Computação."
+                  pic={ufscar}
+                  year="Mar 2013 - Ago 2020"
+                />
+              </FadeIn>
+
+              <FadeIn delay={450} >
+                <HistoricCard
+                  text="Ensino Médio integrado com o Ensino Técnico em Informática.
+                  Neste ponto que escolhi qual carreira seguiria após o colegial."
+                  pic={etec}
+                  year="Mar 2010 - Dez 2012"
+                />
+              </FadeIn>
             </Col>
           </Row>
         </Container>
