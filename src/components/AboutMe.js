743e085d12f0719d8c90439c42708ede00b8fb5a
Guilherme <guilherme.santos@ringa.com.br>
2020-02-04 16:28:35 -0300

    Apply zoom for home page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { CardColumns, Card, Col, Container, Row } from "react-bootstrap";
 import FadeIn from "react-fade-in";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 import Sidebar from "./Sidebar.js";
 
@@ -28,7 +30,14 @@ class AboutMe extends React.Component {
                   <Card className="p-3 text-center">
                     <Card.Title>Bem vindo ao meu site pessoal!</Card.Title>
                     <blockquote className="blockquote mb-0 card-body">
-                      <Card.Img variant="top" src={picture5} />
+                      <Zoom>
+                        <img
+                          alt="Imagem com fundo natalino e a
+                        frente estão o meu filho e a minha esposa"
+                          src={picture5}
+                          width="170"
+                        />
+                      </Zoom>
                       <footer className="blockquote-footer">
                         <small className="text-muted">
                           Tenho 25 anos, sou pai do Leonardo e marido da
@@ -41,7 +50,15 @@ class AboutMe extends React.Component {
 
                 <FadeIn delay={100}>
                   <Card className="text-center">
-                    <Card.Img variant="top" src={picture1} />
+                    <Card.Title></Card.Title>
+                    <Zoom>
+                      <img
+                        alt="Foto de uma arvore com flores amarelas
+                      num dia ensolarado no parque"
+                        src={picture1}
+                        width="170"
+                      />
+                    </Zoom>
                     <Card.Body></Card.Body>
                     <Card.Footer>
                       <small className="text-muted">
@@ -53,7 +70,14 @@ class AboutMe extends React.Component {
 
                 <FadeIn delay={150}>
                   <Card className="text-center">
-                    <Card.Img variant="top" src={picture6} />
+                    <Card.Title></Card.Title>
+                    <Zoom>
+                      <img
+                        alt="Foto de um campo aberto com árvores ao fundo"
+                        src={picture6}
+                        width="170"
+                      />
+                    </Zoom>
                     <Card.Body></Card.Body>
                     <Card.Footer>
                       <small className="text-muted">
@@ -65,7 +89,14 @@ class AboutMe extends React.Component {
 
                 <FadeIn delay={200}>
                   <Card className="text-center">
-                    <Card.Img variant="top" src={picture2} />
+                    <Card.Title></Card.Title>
+                    <Zoom>
+                      <img
+                        alt="Foto de uma montanha em um dia ensolarado"
+                        src={picture2}
+                        width="170"
+                      />
+                    </Zoom>
                     <Card.Body></Card.Body>
                     <Card.Footer>
                       <small className="text-muted">
@@ -88,7 +119,14 @@ class AboutMe extends React.Component {
 
                 <FadeIn delay={300}>
                   <Card className="text-center">
-                    <Card.Img variant="top" src={picture3} />
+                    <Card.Title></Card.Title>
+                    <Zoom>
+                      <img
+                        alt="Foto de um trecho pequeno de queda d'água cristalina"
+                        src={picture3}
+                        width="170"
+                      />
+                    </Zoom>
                     <Card.Body></Card.Body>
                     <Card.Footer>
                       <small className="text-muted">
@@ -102,9 +140,11 @@ class AboutMe extends React.Component {
                   <Card>
                     <Card.Body>
                       <Card.Text>
-                        Resido em São Carlos/SP e algumas dessas fotos foram
-                        tiradas em visitas à cidades vizinhas. Tirar fotos da
-                        natureza é uma das minhas atividades favoritas!
+                        Resido em <b> São Carlos/SP </b> e algumas dessas fotos foram
+                        tiradas em visitas à cidades vizinhas. <br />
+                        <br />
+                        Tirar fotos da natureza é uma das minhas atividades
+                        favoritas!
                       </Card.Text>
                     </Card.Body>
                   </Card>
@@ -112,7 +152,14 @@ class AboutMe extends React.Component {
 
                 <FadeIn delay={400}>
                   <Card className="text-center">
-                    <Card.Img variant="top" src={picture4} />
+                    <Card.Title></Card.Title>
+                    <Zoom>
+                      <img
+                        alt="Foto de uma estrutura de madeira com uma folha ao centro"
+                        src={picture4}
+                        width="170"
+                      />
+                    </Zoom>
                     <Card.Body></Card.Body>
                     <Card.Footer>
                       <small className="text-muted">
@@ -124,7 +171,14 @@ class AboutMe extends React.Component {
 
                 <FadeIn delay={450}>
                   <Card className="text-center">
-                    <Card.Img variant="top" src={picture7} />
+                    <Card.Title></Card.Title>
+                    <Zoom>
+                      <img
+                        alt="Foto de uma árvore de folhas amarelas com nuvens e um céu azul ao fundo"
+                        src={picture7}
+                        width="170"
+                      />
+                    </Zoom>
                     <Card.Body></Card.Body>
                     <Card.Footer>
                       <small className="text-muted">
